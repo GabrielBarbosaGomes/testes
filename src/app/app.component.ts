928726acb73267739234e1416b5917626a84b0de
GabrielBarbosaGomes <gabryel122crf@gmail.com>
2022-10-12 18:44:38 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'drop';
+
+  constructor(private http: HttpClient){}
+
+  file: any;
+  getFile(event: any){
+    this.file = event.target.files[0];
+    console.log("file", this.file);
+  }
+
+  uploadFile(){
+    let formData = new FormData;
+    formData.set("file", this.file);
+
+    //ligando a api
+
+    this.http.post('http://localhost:3001/upload/uploadFiles', formData).subscribe((response: any)=> {});
+  }
 }
